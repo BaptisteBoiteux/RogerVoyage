82207fa815e74cd2713037b52626ab4a1594e281
Julien Mercier <julien.mercier@cpe.fr>
2020-10-15 11:07:06 +0200

    changement du lien de reservation de Caen

---
+++ JS/Identification_prix.js
@@ -0,0 +1,4 @@
+/* cette fonstion permet de rajouter un point d'interogation et un id au lien de reservation */
+function identification(){ 
+  document.getElementById ("Caen").href='Reservation.html?id=Caen';
+}
