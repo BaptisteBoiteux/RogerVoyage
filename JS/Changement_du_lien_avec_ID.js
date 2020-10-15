e4089ba99037d596e57bcb7fc4416bc5ddb4bbde
Julien Mercier <julien.mercier@cpe.fr>
2020-10-15 12:01:23 +0200

    changement du lien de reservation de toute les ville

---
+++ JS/Changement_du_lien_avec_ID.js
@@ -0,0 +1,11 @@
+/* cette fonstion permet de rajouter un point d'interogation et un id au lien de reservation */
+function identification(){ 
+  document.getElementById ("Caen").href='Reservation.html?id=Caen';
+  document.getElementById ("Cherbourg").href='Reservation.html?id=Cherbourg';
+  document.getElementById ("Montfort").href='Reservation.html?id=Montfort';
+  document.getElementById ("Eu").href='Reservation.html?id=Eu';
+  document.getElementById ("Mont-Saint-Michel").href='Reservation.html?id=Mont-Saint-Michel';
+  document.getElementById ("Japon").href='Reservation.html?id=Japon';
+  document.getElementById ("Australie").href='Reservation.html?id=Australie';
+  document.getElementById ("Rouen").href='Reservation.html?id=Rouen';
+}
--- JS/Identification_prix.js
+++
@@ -1,4 +0,0 @@
-/* cette fonstion permet de rajouter un point d'interogation et un id au lien de reservation */
-function identification(){ 
-  document.getElementById ("Caen").href='Reservation.html?id=Caen';
-}
