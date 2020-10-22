7ce108556415505eb4dc86e1ec6561638cce0a99
Baptiste Boiteux <baptiste.boiteux@cpe.fr>
2020-10-22 10:54:56 +0200

    Simplification et ajout du petit_dej

--- JS/Calcul_prix.js
+++ JS/Calcul_prix.js
@@ -1,11 +1,10 @@
 function CalculPrixSejour()
 {
     var PrixPP = 200;
-    var NbAdulte = parseInt(document.getElementById("NbAdulte").value,10);
-    var NbEnfant = parseInt(document.getElementById("NbEnfant").value,10);
-    /*var PetitDej = parseInt(document.getElementById("Petitdej").value,10);*/
-    var PrixTotal = PrixPP*NbAdulte + PrixPP*0.4*NbEnfant;
-    var Affprix = document.getElementById("Prix").innerHTML = "le Prix calculé est : "+PrixTotal;
-    console.log(NbAdulte);
-    console.log(PrixTotal);
+    var PrixPetitDej = 12;
+    var NbAdulte = document.getElementById("NbAdulte").value;
+    var NbEnfant = document.getElementById("NbEnfant").value;
+    var PetitDej = document.getElementById('PetitDej').checked;
+    var PrixTotal = PrixPP*NbAdulte + PrixPP*0.4*NbEnfant + (NbAdulte + NbEnfant)*PetitDej*PrixPetitDej;
+    var Affprix = document.getElementById("Prix").innerHTML = "le Prix calculé est : "+ PrixTotal;
 }
