fa4ecf8bb174c3d624c5132e323405111ffd8859
Baptiste Boiteux <baptiste.boiteux@cpe.fr>
2020-10-22 11:20:06 +0200

    Mis à jour du prix automatiquement sans bouton

--- JS/Calcul_prix.js
+++ JS/Calcul_prix.js
@@ -1,10 +1,11 @@
+const PrixPP = 200;
+const PrixPetitDej = 12;
+
 function CalculPrixSejour()
 {
-    var PrixPP = 200;
-    var PrixPetitDej = 12;
     var NbAdulte = document.getElementById("NbAdulte").value;
     var NbEnfant = document.getElementById("NbEnfant").value;
     var PetitDej = document.getElementById('PetitDej').checked;
     var PrixTotal = PrixPP*NbAdulte + PrixPP*0.4*NbEnfant + (NbAdulte + NbEnfant)*PetitDej*PrixPetitDej;
-    var Affprix = document.getElementById("Prix").innerHTML = "le Prix calculé est : "+ PrixTotal;
+    var Affprix = document.getElementById("Prix").innerHTML = "Prix du Sejour : "+ PrixTotal;
 }
