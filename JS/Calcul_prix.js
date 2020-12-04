7b1be8bbb5aef57f2a636c0ce8dab9fe31109da3
Julien <jm62140@gmail.com>
2020-12-04 18:08:56 +0100

    test connexion

--- JS/Calcul_prix.js
+++ JS/Calcul_prix.js
@@ -1,9 +1,10 @@
 class Destination {
   constructor(prix, titre) {
-    this.prix 		 = prix;
-    this.titre 		 = titre;
+    this.prix      = prix;
+    this.titre     = titre;
   }
 }
+
 const Caen                = new Destination(200,"Caen");
 const Montfort            = new Destination(30,"Montfort");
 const Cherbourg           = new Destination(50,"Cherbourg");
@@ -14,6 +15,8 @@ const Tokyo               = new Destination(300,"Tokyo");
 const Montréal            = new Destination(200,"Montréal");
 const Rouen               = new Destination(35,"Rouen");
 
+let identifiant = 
+'{ "identifiant" : ["titouan_le_s","normandie_fan","darksasuke"],"mdp" : ["12345","jtm_norm","tropdark"]}';
 var DestinationTab = [Caen,Montfort,Cherbourg,Eu,Mont_Saint_Michel,Conteville,Tokyo,Montréal,Rouen];
 
 const PrixPetitDej = 12;
@@ -91,4 +94,36 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+ 
+function test_identité() {
+  let connexion = (JSON.parse(identifiant));
+  var identi = document.getElementById("identité").value;
+  var mot_de_passe = document.getElementById("password").value;
+  var vrai_identité = false;
+  var i=0;
+  while ( i < connexion.identifiant.length && vrai_identité == false)
+  {
+    if (identi == connexion.identifiant[i])
+    {
+      var i_identifiant = i;
+      vrai_identité = true;
+    }
+    i++;
+  }
+  if(vrai_identité)
+  {
+    if(mot_de_passe != connexion.mdp[i_identifiant])
+    {
+      vrai_identité = false;
+    }
+  }
+  if (vrai_identité) 
+  {
+    console.log("bravo titou");
+  }
+  else
+  {
+    console.log("ptdr t'est qui");
+  }
 }
