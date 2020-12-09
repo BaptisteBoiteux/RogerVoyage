c874c2b5008b01b726b6c07b8a3df8e93cf0e1f0
Julien <jm62140@gmail.com>
2020-12-09 23:20:52 +0100

    ordre des ville

--- JS/Script.js
+++ JS/Script.js
@@ -56,14 +56,14 @@ function topFunction() {
 function identification(){ 
   /* cette fonstion permet de rajouter un point d'interogation et un id au lien de reservation */
   document.getElementById ("Caen").href='Reservation.html?id=0';
-  document.getElementById ("Cherbourg").href='Reservation.html?id=2';
   document.getElementById ("Montfort").href='Reservation.html?id=1'; 
-  document.getElementById ("Eu").href='Reservation.html?id=3';
-  document.getElementById ("Mont-Saint-Michel").href='Reservation.html?id=4';
-  document.getElementById ("Conteville").href='Reservation.html?id=5';
+  document.getElementById ("Cherbourg").href='Reservation.html?id=2';
+  document.getElementById ("Mont-Saint-Michel").href='Reservation.html?id=3';
+  document.getElementById ("Conteville").href='Reservation.html?id=4';
+  document.getElementById ("Rouen").href='Reservation.html?id=5';  
   document.getElementById ("Tokyo").href='Reservation.html?id=6';
   document.getElementById ("Montréal").href='Reservation.html?id=7';
-  document.getElementById ("Rouen").href='Reservation.html?id=8';
+  document.getElementById ("Eu").href='Reservation.html?id=8';
 }
 
 
